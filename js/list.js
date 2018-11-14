930cf2d92548aeef66641cad0ea1953bbb2f9ca7
AoDaFang <1426490132@qq.com>
2018-11-14 11:59:18 +0800

    这是完成了第六个页面列表页的版本

--- js/list.js
+++ js/list.js
@@ -76,6 +76,19 @@ requirejs.config({
 
 requirejs(["jquery", "swiper", "baiduT", "do_cookie", "extend", "lazy", "page", "cookie", "top", "head", "nav_s", "er_ji", "xuan_fu", "do_index_cart", "delete_cookie", "fang_da_jing","tab","set_cookie_amount"], function($, Swiper, baidu, do_cookie) {
 	$(".foot").load('html/foot.html');
+//	console.log($(".main_right_middle").offset().top);
+	window.onscroll = function(){//吸顶
+		if($(document).scrollTop() > 261){
+			$(".main_right_middle").css("position","fixed").css("top","-9px").css("z-index","9999");
+		}else{
+			$(".main_right_middle").css("position","")
+		}
+	}
+	
+	$(".main_left dl dt i").click(function(){
+		$(this).parent().next().toggle("active");
+	})
+	
 	
 	$.ajax({
 		type:"get",
@@ -84,14 +97,185 @@ requirejs(["jquery", "swiper", "baiduT", "do_cookie", "extend", "lazy", "page", 
 		dataType:"json",
 		success:function(data){
 			console.log(data)
-			$(".main_right_goods ul").html(baidu.template("temp",data));
+			$(".main_right_goods ul").html("");//清空里面的内容
+			var up_new_data = downSort(data.data,"XiaoLiang");
+			$(".main_right_goods ul").html(baidu.template("temp",up_new_data));//按照销量排序
+			
+			
+			//方家铺子筛选
+			$(".pp_fang_jia").click(function(){
+				$(".main_right_goods ul").html("");//清空里面的内容
+				var up_new_data = isTrue(data.data,"PinPai","方家铺子");
+				$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+				$(".main_right_top").css("display","none");
+				$(".main_right_top_active").css("display","block");
+				$(".main_right_top_active>div>span").html("方家铺子");
+			});
+			
+			//u选筛选
+			$(".pp_uxuan").click(function(){
+				$(".main_right_goods ul").html("");//清空里面的内容
+				var up_new_data = isTrue(data.data,"PinPai","U选一品");
+				$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+				
+				$(".main_right_top").css("display","none");
+				$(".main_right_top_active").css("display","block");
+				$(".main_right_top_active>div>span").html("U选一品");
+			});
+			
+			//什漠果筛选
+			$(".pp_shimo").click(function(){
+				$(".main_right_goods ul").html("");//清空里面的内容
+				var up_new_data = isTrue(data.data,"PinPai","什漠果");
+				$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+				
+				$(".main_right_top").css("display","none");
+				$(".main_right_top_active").css("display","block");
+				$(".main_right_top_active>div>span").html("什漠果");
+			});
+			//品牌筛选的叉号
+			$(".main_right_top_active>div>i").click(function(){
+				$(".main_right_top").css("display","block");
+				$(".main_right_top_active").css("display","none");
+				$(".main_right_goods ul").html("");//清空里面的内容
+				var up_new_data = downSort(data.data,"XiaoLiang");
+				$(".main_right_goods ul").html(baidu.template("temp",up_new_data));//按照销量排序
+			})
+			
+			
+			var bool_up_price = false;//价格按钮是否被激活
+			var jia_ge_isActive = false;//价格按钮是否是升序状态
+			
+			//销量
+			$(".xiao_liang").click(function(){
+				$(".xiao_liang").addClass("active").siblings().removeClass("active");
+				$(".main_right_goods ul").html("");//清空里面的内容
+				var up_new_data = downSort(data.data,"XiaoLiang");
+				$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+				
+				
+				bool_up_price = false;//将价格按钮激活状态去除
+				$(".jia_ge b").removeClass();
+			});
+			
+			
+			//价格
+			$(".jia_ge").click(function(){
+				if(jia_ge_isActive == false){//如果这个按钮还没有被激活，执行升序排序
+					var new_data = upSort(data.data,"sfbestPrice");
+					$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+					$(".jia_ge").addClass("active").siblings().removeClass("active");
+					$(".jia_ge b").removeClass("down").addClass("up");
+					jia_ge_isActive = true;
+					bool_up_price = true;
+				}else{
+					if(bool_up_price == true){//如果按钮被激活，而且正处于升序状态，把它变为降序状态
+						var new_data = downSort(data.data,"sfbestPrice");
+						$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+						$(".jia_ge b").removeClass("up").addClass("down");
+						bool_up_price = false;
+						jia_ge_isActive = true;
+					}else{//如果按钮被激活，而且正处于降序状态，把它变为升序
+						var new_data = upSort(data.data,"sfbestPrice");
+						$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+						$(".jia_ge").addClass("active").siblings().removeClass("active");
+						$(".jia_ge b").removeClass("down").addClass("up");
+						jia_ge_isActive = true;
+						bool_up_price = true;
+					}
+				}
+			});
+			
+			//评论
+			$(".ping_lun").click(function(){
+				$(".ping_lun").addClass("active").siblings().removeClass("active");
+				$(".main_right_goods ul").html("");//清空里面的内容
+				var up_new_data = downSort(data.data,"PingLunShu");
+				$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+				
+				
+				bool_up_price = false;//将价格按钮激活状态去除
+				$(".jia_ge b").removeClass();
+			});
+			
+			//新品
+			$(".xin_pin").click(function(){
+				$(".xin_pin").addClass("active").siblings().removeClass("active");
+				$(".main_right_goods ul").html("");//清空里面的内容
+				var up_new_data = isTrue(data.data,"isNew","1");
+				$(".main_right_goods ul").html(baidu.template("temp",up_new_data));
+				
+				
+				bool_up_price = false;//将价格按钮激活状态去除
+				$(".jia_ge b").removeClass();
+			});
 		},
 		error:function(){
 			console.log(123)
 		}
 	});
 	
+	//判断某个属性是否为真
+	function isTrue(data,prop,value){
+		var new_data = [];
+		$(data).each(function(index,ele){
+			if(ele[prop] == value){
+				new_data.push(ele);
+				
+			}
+		})
+		var goods_amount = new_data.length;
+		$(".main_right_middle_box1_right p>span").html(goods_amount);
+		var up_new_data = {
+			"data":new_data
+		}
+		
+		return up_new_data;
+	}
 	
+	//按照某个属性升序排序
+	function upSort(data,prop){
+		
+		data.sort(function(a,b){
+			if(parseInt(parseInt(a[prop])) > parseInt(b[prop])){
+				return 1;
+			}else if(parseInt(a[prop]) < parseInt(b[prop])){
+				return -1;
+			}else{
+				return 0;
+			}
+		});
+		var goods_amount = data.length;
+		$(".main_right_middle_box1_right p>span").html(goods_amount);
+		
+		var up_new_data = {
+			"data":data
+		}
+		console.log(data)
+		return up_new_data;
+	}
 	
-	
+	//按照某个属性降序排序
+	function downSort(data,prop){
+		
+		data.sort(function(a,b){
+			if(parseInt(parseInt(a[prop])) > parseInt(b[prop])){
+				return -1;
+			}else if(parseInt(a[prop]) < parseInt(b[prop])){
+				return 1;
+			}else{
+				return 0;
+			}
+		});
+		
+		var goods_amount = data.length;
+		$(".main_right_middle_box1_right p>span").html(goods_amount);
+		
+		var up_new_data = {
+			"data":data
+		}
+		console.log(data)
+		return up_new_data;
+	}
+		
 });
